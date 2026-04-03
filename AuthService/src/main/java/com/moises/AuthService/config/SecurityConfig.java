5c975005e322b2460ae68451c037beff3440334f
teyorkk <theomacatienza@gmail.com>
2026-04-03 23:09:32 +0800

    remove strength

--- AuthService/src/main/java/com/moises/AuthService/config/SecurityConfig.java
+++ AuthService/src/main/java/com/moises/AuthService/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
